61f6e78d97579cdbeaf0ae6df6bb9b1053304619
Damirikus <Damir.gazizoff.2011.sta.mail_59@mail.ru>
2022-04-21 13:14:49 +0300

    0333

--- day03/src/ex03/Program.java
+++ day03/src/ex03/Program.java
@@ -2,63 +2,51 @@ package ex03;
 
 import java.io.*;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class Program {
 
     public LinkedList<Thread> listThreads = new LinkedList<>();
-    public ArrayList<String> urls = new ArrayList<>();
-    public Map<Integer, String> urlsReal = new HashMap<>();
+    public static final Object monitor = new Object();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Program program = new Program();
         program.execute();
     }
 
-    public void execute(){
+    public void execute() throws FileNotFoundException {
 
-        int COUNT_OF_THREADS = 4;
+        int COUNT_OF_THREADS = 2;
         String filename = "files_urls.txt";
 
-        try( BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
-//            Files.createDirectory(Paths.get("upload"));
-            while (reader.ready()){
-                String str = reader.readLine();
-
-                String[] arr1 = str.split(" ");
-                urls.add(arr1[1]);
-            }
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+//            if (!Files.isDirectory(Paths.get("upload")));
+//                Files.createDirectory(Paths.get("upload"));
             for (int i = 0; i < COUNT_OF_THREADS; i++){
                 listThreads.add(new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            int numberOfFile = 0;
-                            String path = null;
-                            while (urlsReal.size() == 0);
-//                                wait();
-
-                            synchronized (urlsReal){
-                                for (Integer i : urlsReal.keySet()){
-                                    numberOfFile = i;
+                            while (true){
+                                String s = null;
+                                synchronized (monitor) {
+                                    if (reader.ready())
+                                        s = reader.readLine();
                                 }
-                                path = urlsReal.get(numberOfFile);
-                                urlsReal.remove(numberOfFile);
-                            }
-//                            notify();
-                            System.out.println(Thread.currentThread().getName() + " start download file number " + numberOfFile);
-                            String[] in = path.split("/");
-                            URL url = new URL(path);
-                            InputStream inputStream = url.openStream();
-                            Files.copy(inputStream, new File("upload/" + in[in.length - 1]).toPath());
-                            inputStream.close();
-                            System.out.println(Thread.currentThread().getName() + " finish download file number " + numberOfFile);
-                            synchronized (listThreads){
-                                listThreads.addLast(Thread.currentThread());
+                                if (s == null)
+                                    break;
+                                String[] arr = s.split(" ");
+                                String path = arr[1];
+                                int numberOfFile = Integer.parseInt(arr[0]);
+
+                                System.out.println(Thread.currentThread().getName() + " start download file number " + numberOfFile);
+                                String[] in = path.split("/");
+                                URL url = new URL(path);
+                                InputStream inputStream = url.openStream();
+                                Files.copy(inputStream, new File("upload/" + in[in.length - 1]).toPath());
+                                inputStream.close();
+                                System.out.println(Thread.currentThread().getName() + " finish download file number " + numberOfFile);
                             }
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -67,35 +55,9 @@ public class Program {
                 }));
             }
 
-            int k = 0;
-            for (String u : urls){
-                while (urlsReal.size() == 1){
-//                    wait();
-                }
-
-                synchronized (urlsReal){
-                    urlsReal.put(k + 1, u);
-                }
-
-                Thread thread;
-                synchronized (listThreads){
-                    while (listThreads.size() == 0);
-                    thread = listThreads.removeFirst();
-                }
+            for (Thread thread : listThreads){
                 thread.start();
-
-                k++;
             }
-
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
-
-
 
 }
