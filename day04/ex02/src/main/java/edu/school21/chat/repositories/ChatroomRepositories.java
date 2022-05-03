8a65b89a6822e7a73c317db956c644180b28756f
Damirikus <Damir.gazizoff.2011.sta.mail_59@mail.ru>
2022-05-03 10:19:24 +0300

    almost ok

--- day04/ex02/src/main/java/edu/school21/chat/repositories/ChatroomRepositories.java
+++
@@ -1,9 +0,0 @@
-package edu.school21.chat.repositories;
-
-import edu.school21.chat.models.Chatroom;
-
-import java.util.Optional;
-
-public interface ChatroomRepositories {
-    Optional<Chatroom> findById(Long id);
-}
